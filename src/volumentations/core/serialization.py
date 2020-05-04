4c4871aca1a779a78db1db7f5b085332dd474964
kumuji <alexey.nekrasov@rwth-aachen.de>
2020-05-05 01:33:01 +0200

    Formating

--- src/volumentations/core/serialization.py
+++ src/volumentations/core/serialization.py
@@ -3,15 +3,14 @@ from __future__ import absolute_import
 import json
 import warnings
 
+from volumentations import __version__
+
 try:
     import yaml
 
     yaml_available = True
 except ImportError:
     yaml_available = False
-
-
-from volumentations import __version__
 
 
 __all__ = ["to_dict", "from_dict", "save", "load"]
@@ -22,8 +21,9 @@ SERIALIZABLE_REGISTRY = {}
 
 class SerializableMeta(type):
     """
-    A metaclass that is used to register classes in `SERIALIZABLE_REGISTRY` so they can be found later
-    while deserializing transformation pipeline using classes full names.
+    A metaclass that is used to register classes in `SERIALIZABLE_REGISTRY`
+    so they can be found later while deserializing transformation pipeline
+    using classes full names.
     """
 
     def __new__(cls, name, bases, class_dict):
@@ -34,20 +34,21 @@ class SerializableMeta(type):
 
 def to_dict(transform, on_not_implemented_error="raise"):
     """
-    Take a transform pipeline and convert it to a serializable representation that uses only standard
-    python data types: dictionaries, lists, strings, integers, and floats.
+    Take a transform pipeline and convert it to a serializable representation
+    that uses only standard python data types: dictionaries, lists, strings,
+    integers, and floats.
 
     Args:
-        transform (object): A transform that should be serialized. If the transform doesn't implement the `to_dict`
-            method and `on_not_implemented_error` equals to 'raise' then `NotImplementedError` is raised.
-            If `on_not_implemented_error` equals to 'warn' then `NotImplementedError` will be ignored
+        transform (object): A transform that should be serialized.
+            If the transform doesn't implement the `to_dict` method and `on_not_implemented_error`
+            equals to 'raise' then `NotImplementedError` is raised. If `on_not_implemented_error`
+            equals to 'warn' then `NotImplementedError` will be ignored
             but no transform parameters will be serialized.
     """
     if on_not_implemented_error not in {"raise", "warn"}:
         raise ValueError(
-            "Unknown on_not_implemented_error value: {}. Supported values are: 'raise' and 'warn'".format(
-                on_not_implemented_error
-            )
+            "Unknown on_not_implemented_error value: {}. Supported values are:"
+            "'raise' and 'warn'".format(on_not_implemented_error)
         )
     try:
         transform_dict = transform._to_dict()  # skipcq: PYL-W0212
@@ -57,8 +58,9 @@ def to_dict(transform, on_not_implemented_error="raise"):
 
         transform_dict = {}
         warnings.warn(
-            "Got NotImplementedError while trying to serialize {obj}. Object arguments are not preserved. "
-            "Implement either '{cls_name}.get_transform_init_args_names' or '{cls_name}.get_transform_init_args' "
+            "Got NotImplementedError while trying to serialize {obj}. Object arguments "
+            "are not preserved. Implement either '{cls_name}.get_transform_init_args_names' "
+            "or '{cls_name}.get_transform_init_args' "
             "method to make the transform serializable".format(
                 obj=transform, cls_name=transform.__class__.__name__
             )
@@ -71,8 +73,9 @@ def instantiate_lambda(transform, lambda_transforms=None):
         name = transform["__name__"]
         if lambda_transforms is None:
             raise ValueError(
-                "To deserialize a Lambda transform with name {name} you need to pass a dict with this transform "
-                "as the `lambda_transforms` argument".format(name=name)
+                "To deserialize a Lambda transform with name {name} you need to pass "
+                "a dict with this transform "
+                "as the `lambda_transforms` argument".format(name=name),
             )
         transform = lambda_transforms.get(name)
         if transform is None:
@@ -89,10 +92,11 @@ def from_dict(transform_dict, lambda_transforms=None):
     """
     Args:
         transform (dict): A dictionary with serialized transform pipeline.
-        lambda_transforms (dict): A dictionary that contains lambda transforms, that is instances of the Lambda class.
-            This dictionary is required when you are restoring a pipeline that contains lambda transforms. Keys
-            in that dictionary should be named same as `name` arguments in respective lambda transforms from
-            a serialized pipeline.
+        lambda_transforms (dict): A dictionary that contains lambda transforms,
+            that is instances of the Lambda class. This dictionary is required
+            when you are restoring a pipeline that contains lambda transforms. Keys
+            in that dictionary should be named same as `name` arguments
+            in respective lambda transforms from a serialized pipeline.
     """
     transform = transform_dict["transform"]
     lmbd = instantiate_lambda(transform, lambda_transforms)
@@ -127,8 +131,9 @@ def save(transform, filepath, data_format="json", on_not_implemented_error="rais
         transform (obj): Transform to serialize.
         filepath (str): Filepath to write to.
         data_format (str): Serialization format. Should be either `json` or 'yaml'.
-        on_not_implemented_error (str): Parameter that describes what to do if a transform doesn't implement
-            the `to_dict` method. If 'raise' then `NotImplementedError` is raised, if `warn` then the exception will be
+        on_not_implemented_error (str): Parameter that describes what to do if
+            a transform doesn't implement the `to_dict` method. If 'raise'
+            then `NotImplementedError` is raised, if `warn` then the exception will be
             ignored and no transform arguments will be saved.
     """
     check_data_format(data_format)
@@ -148,10 +153,11 @@ def load(filepath, data_format="json", lambda_transforms=None):
         transform (obj): Transform to serialize.
         filepath (str): Filepath to read from.
         data_format (str): Serialization format. Should be either `json` or 'yaml'.
-        lambda_transforms (dict): A dictionary that contains lambda transforms, that is instances of the Lambda class.
-            This dictionary is required when you are restoring a pipeline that contains lambda transforms. Keys
-            in that dictionary should be named same as `name` arguments in respective lambda transforms from
-            a serialized pipeline.
+        lambda_transforms (dict): A dictionary that contains lambda transforms,
+            that is instances of the Lambda class. This dictionary is required
+            when you are restoring a pipeline that contains lambda transforms. Keys
+            in that dictionary should be named same as `name` arguments in respective
+            lambda transforms from a serialized pipeline.
     """
     check_data_format(data_format)
     load_fn = json.load if data_format == "json" else yaml.safe_load
