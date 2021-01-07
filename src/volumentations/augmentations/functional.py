Added correct flip, rotation around point

src/volumentations/augmentations/functional.py
@@ -8,13 +8,14 @@ def scale(points, scale_factor=(1, 1, 1)):
     return points
 
 
-def rotate_around_axis(points, axis, angle):
+def rotate_around_axis(points, axis, angle, center_point=None):
     """
     Return the rotation matrix associated with counterclockwise rotation about
     the given axis by angle in radians.
     https://stackoverflow.com/questions/6802577/rotation-of-3d-vector
     """
-    center_point = points[:, :3].mean(axis=0).astype(points[:, :3].dtype)
+    if center_point is None:
+        center_point = points[:, :3].mean(axis=0).astype(points[:, :3].dtype)
     axis = axis / np.sqrt(np.dot(axis, axis))
     a = np.cos(angle / 2.0)
     b, c, d = -axis * np.sin(angle / 2.0)
@@ -69,3 +70,50 @@ def center(points, origin=(0, 0, 0)):
 def move(points, offset=(0, 0, 0)):
     points[:, :3] = points[:, :3] + offset
     return points
+
+
+def flip_coordinates(points, axis):
+    coord_max = np.max(points[:, axis])
+    points[:, axis] = coord_max - points[:, axis]
+    return points
+
+
+# import scipy
+
+# def elastic_distortion(pointcloud, granularity, magnitude):
+#     """Apply elastic distortion on sparse coordinate space.
+
+#     pointcloud: numpy array of (number of points, at least 3 spatial dims)
+#     granularity: size of the noise grid (in same scale[m/cm] as the voxel grid)
+#     magnitude: noise multiplier
+#   """
+#     blurx = np.ones((3, 1, 1, 1)).astype("float32") / 3
+#     blury = np.ones((1, 3, 1, 1)).astype("float32") / 3
+#     blurz = np.ones((1, 1, 3, 1)).astype("float32") / 3
+#     coords = pointcloud[:, :3]
+#     coords_min = coords.min(0)
+
+#     # Create Gaussian noise tensor of the size given by granularity.
+#     noise_dim = ((coords - coords_min).max(0) // granularity).astype(int) + 3
+#     noise = np.random.randn(*noise_dim, 3).astype(np.float32)
+
+#     # Smoothing.
+#     for _ in range(2):
+#         noise = scipy.ndimage.filters.convolve(noise, blurx, mode="constant", cval=0)
+#         noise = scipy.ndimage.filters.convolve(noise, blury, mode="constant", cval=0)
+#         noise = scipy.ndimage.filters.convolve(noise, blurz, mode="constant", cval=0)
+
+#     # Trilinear interpolate noise filters for each spatial dimensions.
+#     ax = [
+#         np.linspace(d_min, d_max, d)
+#         for d_min, d_max, d in zip(
+#             coords_min - granularity,
+#             coords_min + granularity * (noise_dim - 2),
+#             noise_dim,
+#         )
+#     ]
+#     interp = scipy.interpolate.RegularGridInterpolator(
+#         ax, noise, bounds_error=0, fill_value=0
+#     )
+#     pointcloud[:, :3] = coords + interp(coords) * magnitude
+#     return pointcloud

tests/test_augmentations.py
@@ -20,7 +20,7 @@ from volumentations import (
         [Move3d, {}],
         [RandomDropout3d, {"dropout_ratio": 0.0}],
         [Crop3d, {}],
-        [NoOp, {}]
+        [NoOp, {}],
     ],
 )
 def test_augmentations_wont_change_input(
@@ -32,28 +32,6 @@ def test_augmentations_wont_change_input(
     normals_copy = normals.copy()
     aug = augmentation_cls(p=1, **params)
     data = aug(points=points, features=features, labels=labels, normals=normals,)
-    np.testing.assert_allclose(data["points"], points_copy)
-    np.testing.assert_allclose(data["features"], features_copy)
-    np.testing.assert_allclose(data["labels"], labels_copy)
-    np.testing.assert_allclose(data["normals"], normals_copy)
-
-
-def test_augmentations_flip_wont_change_input(
-    points, features, labels, normals, bboxes, cameras
-):
-    points_copy = points.copy()
-    features_copy = features.copy()
-    labels_copy = labels.copy()
-    normals_copy = normals.copy()
-    data = Flip3d(p=1)(
-        points=points, features=features, labels=labels, normals=normals,
-    )
-    data = Flip3d(p=1)(
-        points=data["points"],
-        features=data["features"],
-        labels=data["labels"],
-        normals=data["normals"],
-    )
     np.testing.assert_allclose(data["points"], points_copy)
     np.testing.assert_allclose(data["features"], features_copy)
     np.testing.assert_allclose(data["labels"], labels_copy)